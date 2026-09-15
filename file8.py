ba913ad41321b739274d1acea02bb5b23035a4b6
coderpranav1404 <kurhadepranav1404@gmail.com>
2026-09-15 12:14:10 +0530

    added file8

---
+++ file8.py
@@ -0,0 +1,19 @@
+#....init...functions
+
+class student:
+    name="Pranita"
+
+s1=student()
+print(s1.name)
+
+#parametirized constructors
+class student:
+
+ def __init__(self,name,marks):
+    self.name=name
+    self.marks=marks
+
+
+
+s1=student("Pranita", 99)
+print(s1.name,s1.marks)
