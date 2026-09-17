a838c83f9ee6b3fdb4b093973960c2087af7a63d
coderpranav1404 <314154734+code-with-pranita@users.noreply.github.com>
2026-09-17 21:01:50 +0530

    methods

--- file8.py
+++ file8.py
@@ -17,3 +17,18 @@ class student:
 
 s1=student("Pranita", 99)
 # print(s1.name,s1.marks)
+#similarly you can create s2
+
+# Methods
+#class student:
+
+ #def __init__(self,name,marks):
+
+   ### self.name=name
+    #self.marks=marks
+
+def welcome(self):
+   print("welcome students",s1.name)
+
+s1=student("Pranita", 99)
+s1.welcome()
