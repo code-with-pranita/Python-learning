34bb354dbbe2a4fb548fa5687184f3c50039dc3b
coderpranav1404 <kurhadepranav1404@gmail.com>
2026-09-19 11:25:13 +0530

    Upload new file

---
+++ file9.py
@@ -0,0 +1,21 @@
+#for loop
+
+# 1) count vowels in str
+
+str=input("Enter a string")
+count=0
+for el in str:
+    if (el=="a" or el=="e" or el=="i" or el=="o" or el=="u"):
+     count+=1
+
+
+print("the",count)
+
+
+# 2) square of num from 1 to 10
+for i in range(1,11):
+   print(i*i)
+    #  or
+num= (1,2,3,4,5,6,7,8,9,10)
+for el in num:
+   print(el*el)
