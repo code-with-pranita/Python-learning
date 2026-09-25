9d01978cfbab15e49d0accf94ee7b497549712cd
code-with-pranita <kurhadepranita651@gmail.com>
2026-09-25 16:27:07 +0530

    Questions

--- file9.py
+++ file9.py
@@ -11,3 +11,17 @@ str=input("write")
 
 for el in str:
       print(el,str.count(el))
+
+      # 5) G A M E
+
+import random
+secret= random.randint(1,10) # Both 1 ,10 are included or you may use random.randrange(1,11)
+guess=int(input("write a no."))
+if(secret==guess):
+   print("you won")
+else:
+   print("you lose")
+
+# 6) reverse a string
+str= "victory and"
+print(str[::-1])
