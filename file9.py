85cf17d3d7250bdbcde7a93fb8e8fb4d39570555
code-with-pranita <kurhadepranita651@gmail.com>
2026-09-26 17:45:40 +0530

    Questions

--- file9.py
+++ file9.py
@@ -1,18 +1,4 @@
- # 3) sum of all no. from 1 to 100
-sum=0
-for el in range(1,101):
- sum+=el
-print("sum=",sum)
-
- # 4) clculate freq of each words in string
-
-str=input("write")
-
-
-for el in str:
-      print(el,str.count(el))
-
-      # 5) G A M E
+# 5) G A M E
 
 import random
 secret= random.randint(1,10) # Both 1 ,10 are included or you may use random.randrange(1,11)
@@ -25,33 +11,3 @@ else:
 # 6) reverse a string
 str= "victory and"
 print(str[::-1])
-
-# To remove duplicate from lists
-info=[2,3,6,2,4]
-print(set(info))
-
-#to arrange in ascending
-
-info.sort()
-print(info)
-
-#Year is leap or not
-year=int(input("type year"))
-if(year%400==0)or(year%4==0)and(year%100!=0):
-   print("leap year")
-else:
-   print("nonleap year")
-
-#to find second largest no. in list
-list=[3,5,8,6]
-list.sort()
-print(list[-2])
-
-#To add at last
-a=[4,6,9]
-a.append(6)
-print(a)
-#To remove
-a=[4,6,9]
-a.remove(9)#removes a value in list only
-print(a)
